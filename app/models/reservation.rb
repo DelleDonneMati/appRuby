2d1c2111da2e0fb947b23b22645fee80bb1acdcc
DelleDonneMati <matiasdeledone@hotmail.com.ar>
2020-01-07 14:14:39 -0300

    Reservas

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,11 +3,14 @@ class Reservation < ApplicationRecord
   belongs_to :user
 
   def self.findById(id)
-    @connection = ActiveRecord::Base.connection
-    result = @connection.exec_query("SELECT *
-                                       FROM reservations
-                                       WHERE id='#{id}'")
-    return result[0]
+    # @connection = ActiveRecord::Base.connection
+    # result = @connection.exec_query("SELECT *
+    #                                    FROM reservations
+    #                                    WHERE id='#{id}'")
+    # return result[0]
+    reserv = Reservation
+     .select("reservations.*")
+     .where("reservations.id = '#{id}'")
   end
 
 end
